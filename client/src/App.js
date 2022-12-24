b03b4ba2334ed9a9e169c13d496b4a21e7a58be5
Mohsin Ali <59974279+imohsinali@users.noreply.github.com>
2022-12-24 20:42:31 +0500

    template for any dapp

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
-
+import {ethers} from 'ethers'
+import {useState,useEffect} from 'react'
+import abi from './contract/chai.json'
 function App() {
+  const [state,setState]=useState({
+    provider:null,
+    singner:null,
+    contract:null,
+
+  })
+  useEffect(()=>{
+    const connectWallect=async ()=>{
+      const contractAddress="0xf6b1037C2f30DAD5cD7E1422a56222beC7C7A7dD"
+      const contractAbi=abi.abi
+      try {
+        const {ethereum} =window
+        if(ethereum)
+        {
+          const account=await ethereum.request({
+            method:"eth_requestAccounts",
+          })
+        }
+        const provider=new ethers.providers.Web3Provider(ethereum)
+        const signer=provider.getSigner()
+        const contract=new ethers.Contract(contractAddress,contractAbi,signer)
+        setState({provider,signer,contract})
+      } catch (error) {
+        console.log(error)
+      }
+
+    }
+    connectWallect()
+  },[])
+  console.log(state)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    hello
     </div>
   );
 }
